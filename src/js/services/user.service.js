44f14fbcdce272d42dd0d5cabb53dbdcc9e0cecc
Bruce Kung <bruce.kung34@gmail.com>
2016-05-17 23:57:13 -0400

    Add JWT auth

--- src/js/services/user.service.js
+++ src/js/services/user.service.js
@@ -1,8 +1,11 @@
 export default class User {
-    constructor(AppConstants, $http) {
+    constructor(AppConstants, $http, $state) {
         'ngInject';
+
+        this._JWT = JWT;
         this._AppConstants = AppConstants;
         this._$http = $http;
+        this._$state = $state;
 
         // Object to store our user properties
         this.current = null;
@@ -20,12 +23,22 @@ export default class User {
         }).then(
             // On success...
             (res) => {
+                // Set the JWT token
+                this._JWT.save(res.data.user.token);
+
                 // Store the user's info for easy lookup
                 this.current = res.data.user;
 
                 return res;
             }
         );
+    }
+
+    logout() {
+      this.current = null;
+      this._JWT.destroy();
+      // Do a hard reload of current state to ensure all data is flushed
+      this._$state.go(this._$state.$current, null, { reload: true});
     }
 
 }
